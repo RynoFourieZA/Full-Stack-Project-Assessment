9f05d61d206c762e16e62b975d709502204e0c52
Ryno <rjrfourie@outlook.com>
2021-08-05 23:51:19 +0200

    Use span for text

--- client/src/Video.js
+++ client/src/Video.js
@@ -1,39 +1,42 @@
-import React, {useState} from "react";
-import ReactPlayer from "react-player"
+import React, { useState } from "react";
+import ReactPlayer from "react-player";
 
-const Video = ({response}) => {
-    let [upVote, setUpVote] = useState(0);
-    let [downVote, setDownVote] = useState(0);
+const Video = ({ response }) => {
+  let [upVote, setUpVote] = useState(0);
+  let [downVote, setDownVote] = useState(0);
 
-    function like() {
-        setUpVote(upVote + 1);
-    }
-    function dislike() {
-        setDownVote(downVote + 1);
-    }
-    return (
-        <div className="container">
-        {response.map(data => {
-            return (
-                <div className="video-block">
-                    <h1>{data.title}</h1>
-                    <div className="video-block">
-                        <ReactPlayer 
-                        className='react-player'
-                        url={data.url} />
-                    </div>
-                    <div className="video-bar">
-                        <p>Rating: {data.rating}</p>
-                        <button>Delete</button>
-                        <button onClick={like}>Up Vote</button> 
-                        <p>{upVote} </p>
-                        <button onClick={dislike}>Down Vote</button> 
-                        <p>{downVote} </p>
-                    </div>
-                </div>
-            )
-        })}
-        </div>
-    )}
+  function like() {
+    setUpVote(upVote + 1);
+  }
+  function dislike() {
+    setDownVote(downVote + 1);
+  }
+  return (
+    <div className="container">
+      {response.map((data) => {
+        return (
+          <div className="video-block">
+            <h1>{data.title}</h1>
+            <div className="video-block">
+              <ReactPlayer className="react-player" url={data.url} />
+            </div>
+            <div className="row">
+              <span>Rating: {data.rating}</span>
+              <button className="btn btn-danger">Delete</button>
+              <button className="btn btn-warning" onClick={like}>
+                Up Vote
+              </button>
+              <span>{upVote} </span>
+              <button className="btn btn-primary" onClick={dislike}>
+                Down Vote
+              </button>
+              <span>{downVote} </span>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
 
 export default Video;
